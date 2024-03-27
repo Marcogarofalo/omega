ccc8ce5dd98a58cd984c0bc80c026aad64c8464d
Marcogarofalo <marcogarofalo31@gmail.com>
2024-03-27 12:02:24 +0100

    move to qbig

---
+++ collect_diagrams/read_topo.py
@@ -0,0 +1,4 @@
+
+
+def read_topo_TD(dir,i1mom,i2mom2,i3mom, j1mom,j2mom, j3mom  )
+    np.load(dir+'000/F48P30/one/one-one-pi/'+conf+'/corr_onepi_conf_'+conf+'_gi_0_gj_0_mom_'+imom_twop+'_src_'+str(s)+'.npy')
